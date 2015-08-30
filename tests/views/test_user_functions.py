5058017c92fe06baf0c7f8f7a8b55dc72795ee27
PrahlM93 <matt_prahl@live.com>
2015-08-30 14:41:29 -0400

    Added Login Tests

---
+++ tests/views/test_user_functions.py
@@ -0,0 +1,13 @@
+﻿from swagmail import app
+
+class TestUserFunctions:
+
+    def test_user_login(self, loggedin_client):
+        rv = loggedin_client.get("/", follow_redirects=True)
+        print rv.data
+        assert "Logout" in rv.data
+
+
+    def test_user_logout(self, loggedin_client):
+        rv = loggedin_client.get("/logout", follow_redirects=True)
+        assert "Successfully logged out" in rv.data
