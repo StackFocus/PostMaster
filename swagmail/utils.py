e10872fa103e58cfddbd7c077c8447c22f11430d
PrahlM93 <matt_prahl@live.com>
2015-09-12 20:48:02 -0400

    Changed the MailDB functions to not be part of a class
    Changed the MailDB functions to not be part of a class
    Added tests for the functions in utils.py

--- swagmail/utils.py
+++ swagmail/utils.py
@@ -14,194 +14,184 @@ class ValidationError(Exception):
     """
     pass
 
-class MailDb(object):
-    """ A class for for wrapping the SQLAlchemy models
-    VirtualDomains, VirtualUsers, and Virtual Aliases
+
+def getAllFromTable(table):
+    """ Returns the entire table that is specified
     """
 
-    def __init__(self):
-        pass
+    # Dynamically get the model to query based on the table variable
+    tableModel = getattr(models, table)
 
+    if tableModel:
+        items = tableModel.query.all()
+        itemsDict = []
 
-    def getAllFromTable(self, table): # pylint: disable=no-self-use
-        """ Returns the entire table that is specified
-        """
-
-        # Dynamically get the model to query based on the table variable
-        tableModel = getattr(models, table)
-
-        if tableModel:
-            items = tableModel.query.all()
-            itemsDict = []
-
-            for item in items:
-                itemDict = []
-                itemDict = item.__dict__
-                # Remove the unnecessary SQLAlchemy key
-                itemDict.pop('_sa_instance_state', None)
-                itemsDict.append(itemDict)
-
-            return itemsDict
-
-        return None
-
-
-    def getDomain(self, domain): # pylint: disable=no-self-use
-        """ Returns a domain from VirtualDomains
-        """
-
-        queriedDomain = models.VirtualDomains.query.filter_by(name=domain).first()
-        if queriedDomain:
-            queriedDomainDict = queriedDomain.__dict__
+        for item in items:
+            itemDict = []
+            itemDict = item.__dict__
             # Remove the unnecessary SQLAlchemy key
-            queriedDomainDict.pop('_sa_instance_state', None)
-            return queriedDomainDict
+            itemDict.pop('_sa_instance_state', None)
+            itemsDict.append(itemDict)
+
+        return itemsDict
+
+    return None
 
 
-    def getDomains(self):
-        """ Returns all domains from VirtualDomains
-        """
+def getDomain(domain):
+    """ Returns a domain from VirtualDomains
+    """
 
-        return self.getAllFromTable('VirtualDomains')
-
-
-    def addDomain(self, domain): # pylint: disable=no-self-use
-        """ Adds a domain to VirtualDomains
-        """
-
-        if not self.getDomain(domain):
-
-            try:
-                db.session.add(models.VirtualDomains(domain))
-                db.session.commit()
-                return True
-            except:
-                db.session.rollback()
-                raise
-        else:
-            raise ValidationError('The domain "%s" already exists' % domain)
-
-        return False
+    queriedDomain = models.VirtualDomains.query.filter_by(name=domain).first()
+    if queriedDomain:
+        queriedDomainDict = queriedDomain.__dict__
+        # Remove the unnecessary SQLAlchemy key
+        queriedDomainDict.pop('_sa_instance_state', None)
+        return queriedDomainDict
 
 
-    def getUser(self, email): # pylint: disable=no-self-use
-        """ Returns a user from VirtualUsers
-        """
+def getDomains():
+    """ Returns all domains from VirtualDomains
+    """
 
-        queriedUser = models.VirtualUsers.query.filter_by(email=email).first()
-
-        if queriedUser:
-            queriedUserDict = queriedUser.__dict__
-            # Remove the unnecessary SQLAlchemy key
-            queriedUserDict.pop('_sa_instance_state', None)
-            return queriedUserDict
-
-        return None
+    return getAllFromTable('VirtualDomains')
 
 
-    def getUsers(self):
-        """ Returns all users from VirtualUsers
-        """
+def addDomain(domain):
+    """ Adds a domain to VirtualDomains
+    """
 
-        return self.getAllFromTable('VirtualUsers')
+    if not getDomain(domain):
+
+        try:
+            db.session.add(models.VirtualDomains(domain))
+            db.session.commit()
+            return True
+        except:
+            db.session.rollback()
+            raise
+    else:
+        raise ValidationError('The domain "%s" already exists' % domain)
+
+    return False
 
 
-    def addUser(self, email, password):
-        """ Adds a user to VirtualUsers
-        """
+def getUser(email):
+    """ Returns a user from VirtualUsers
+    """
 
-        if not self.getUser(email):
+    queriedUser = models.VirtualUsers.query.filter_by(email=email).first()
 
-            # Check to see if the domain can be extracted and if the email is at least somewhat in the right format
-            if search('(?<=@).*$', email) and match('[a-z].*@.*[.].*[a-z]$', email):
-                domain = search('(?<=@).*$', email).group(0)
-                domainRow = self.getDomain(domain)
+    if queriedUser:
+        queriedUserDict = queriedUser.__dict__
+        # Remove the unnecessary SQLAlchemy key
+        queriedUserDict.pop('_sa_instance_state', None)
+        return queriedUserDict
 
-                if domainRow and 'id' in domainRow:
-                    salt = (sha1(urandom(16)).hexdigest())[:16]
-                    passwordAndSalt = sha512.encrypt(password, rounds=5000,
-                                                        salt=salt, implicit_rounds=True)
-                    try:
-                        newUser = models.VirtualUsers(domainRow['id'], passwordAndSalt, email)
-                        db.session.add(newUser)
-                        db.session.commit()
-                        return True
+    return None
 
-                    except:
-                        db.session.rollback()
-                        raise
-                else:
-                    raise ValidationError('The domain "%s" is not managed by this database' % domain)
+
+def getUsers():
+    """ Returns all users from VirtualUsers
+    """
+
+    return getAllFromTable('VirtualUsers')
+
+
+def addUser(email, password):
+    """ Adds a user to VirtualUsers
+    """
+
+    if not getUser(email):
+
+        # Checks if the domain can be extracted and if the email is at least somewhat in the right format
+        if search('(?<=@).*$', email) and match('[a-z].*@.*[.].*[a-z]$', email):
+            domain = search('(?<=@).*$', email).group(0)
+            domainRow = getDomain(domain)
+
+            if domainRow and 'id' in domainRow:
+                salt = (sha1(urandom(16)).hexdigest())[:16]
+                passwordAndSalt = sha512.encrypt(password, rounds=5000,
+                                                 salt=salt, implicit_rounds=True)
+                try:
+                    newUser = models.VirtualUsers(domainRow['id'], passwordAndSalt, email)
+                    db.session.add(newUser)
+                    db.session.commit()
+                    return True
+
+                except:
+                    db.session.rollback()
+                    raise
             else:
-                raise ValidationError('"%s" is not a valid email address' % email)
+                raise ValidationError('The domain "%s" is not managed by this database' % domain)
         else:
-            raise ValidationError('The user "%s" already exists' % email)
+            raise ValidationError('"%s" is not a valid email address' % email)
+    else:
+        raise ValidationError('The user "%s" already exists' % email)
 
-        return False
-
-
-    def getAlias(self, source): # pylint: disable=no-self-use
-        """ Returns alias from VirtualAliases
-        """
-
-        queriedAlias = models.VirtualAliases.query.filter_by(source=source).first()
-
-        if queriedAlias:
-            queriedAliasDict = queriedAlias.__dict__
-            # Remove the unnecessary SQLAlchemy key
-            queriedAliasDict.pop('_sa_instance_state', None)
-            return queriedAliasDict
-
-        return None
+    return False
 
 
-    def getAliases(self):
-        """ Returns all aliases from VirtualAliases
-        """
-        return self.getAllFromTable('VirtualAliases')
+def getAlias(source):
+    """ Returns alias from VirtualAliases
+    """
+
+    queriedAlias = models.VirtualAliases.query.filter_by(source=source).first()
+
+    if queriedAlias:
+        queriedAliasDict = queriedAlias.__dict__
+        # Remove the unnecessary SQLAlchemy key
+        queriedAliasDict.pop('_sa_instance_state', None)
+        return queriedAliasDict
+
+    return None
 
 
-    def addAlias(self, source, destination):
-        """ Adds an alias to VirtualAliases
-        """
+def getAliases():
+    """ Returns all aliases from VirtualAliases
+    """
+    return getAllFromTable('VirtualAliases')
 
-        if not self.getAlias(source):
 
-            sourceDomain = search('(?<=@).*$', source).group(0)
-            destinationDomain = search('(?<=@).*$', destination).group(0)
+def addAlias(source, destination):
+    """ Adds an alias to VirtualAliases
+    """
 
-            if sourceDomain == destinationDomain:
+    if not getAlias(source):
 
-                if match('.*@.*[.].*[a-z]$', source):
+        sourceDomain = search('(?<=@).*$', source).group(0)
+        destinationDomain = search('(?<=@).*$', destination).group(0)
 
-                    if self.getUser(destination):
-                        domainRow = self.getDomain(destinationDomain)
+        if sourceDomain == destinationDomain:
 
-                        if domainRow and 'id' in domainRow:
-                        
-                            try:
-                                alias = models.VirtualAliases(domainRow['id'], source, destination)
-                                db.session.add(alias)
-                                db.session.commit()
-                                return True
-                        
-                            except:
-                                db.session.rollback()
-                                raise
+            if match('.*@.*[.].*[a-z]$', source):
 
-                        else:
-                            raise ValidationError \
-                                ('The domain "%s" is not managed by this database' % sourceDomain)
+                if getUser(destination):
+                    domainRow = getDomain(destinationDomain)
+
+                    if domainRow and 'id' in domainRow:
+                        try:
+                            alias = models.VirtualAliases(domainRow['id'], source, destination)
+                            db.session.add(alias)
+                            db.session.commit()
+                            return True
+
+                        except:
+                            db.session.rollback()
+                            raise
 
                     else:
                         raise ValidationError \
-                            ('The destination "%s" is not a current email address' % destination)
+                            ('The domain "%s" is not managed by this database' % sourceDomain)
+
                 else:
-                    raise ValidationError('The source "%s" is not in a valid email format' % source)
+                    raise ValidationError \
+                        ('The destination "%s" is not a current email address' % destination)
             else:
-                raise ValidationError('The domains from the source and destination alias do not match')
+                raise ValidationError('The source "%s" is not in a valid email format' % source)
         else:
-            raise ValidationError('The alias "%s" already exists' % source)
+            raise ValidationError('The domains from the source and destination alias do not match')
+    else:
+        raise ValidationError('The alias "%s" already exists' % source)
 
-        return False
-
+    return False
