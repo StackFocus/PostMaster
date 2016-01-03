ce883ce7306a6b56401bb98515175e5926dc9cc4
PrahlM93 <matt_prahl@live.com>
2016-01-02 21:41:18 -0500

    Enhanced Responsive Tables
    * Enhanced the responsive tables by using the "No More Tables" method.
    * Removed the cancel button when in phone size on the x-editable form to
    allow for more room of the input box
    * Fixed the login panel position

--- swagmail/static/js/login.js
+++ swagmail/static/js/login.js
@@ -2,7 +2,13 @@
     var bodyHeight = $('#body').height();
     var navHeight = $('#navigation-bar').height();
     var loginRowHeight = $('#loginRow').height();
-    $('#loginRow').css('margin-top', (bodyHeight - navHeight - loginRowHeight) / 3);
+    var marginTop = (bodyHeight - navHeight - loginRowHeight) / 3
+    if (marginTop > 10) {
+        $('#loginRow').css('margin-top', marginTop);
+    }
+    else {
+        $('#loginRow').css('margin-top', 10);
+    }
 }
 
 $(document).ready(function () {
