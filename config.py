2dab48bd90e9872eeb19432c8891b8902806b9dc
PrahlM93 <matt_prahl@live.com>
2016-01-09 19:07:41 -0500

    Small fix

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from os import path
 HOST_NAME = 'mail.swagger.pro'
 # Make this random (used to generate session keys)
 SECRET_KEY = '73Sjkadas%hASDe45@1238Due12jdodjfFDweq82348*(#89asdasdas'
-basedir = os.path.abspath(path.dirname(__file__))
+basedir = path.abspath(path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + path.join(basedir, 'swagmail.db')
 SQLALCHEMY_MIGRATE_REPO = path.join(basedir, 'db_repository')
 
