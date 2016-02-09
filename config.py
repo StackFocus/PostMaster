17ea6540bc50c1012a96418824764220254fffbb
PrahlM93 <matt_prahl@live.com>
2016-02-08 19:25:30 -0500

    Remove DEBUG in config.py
    * Since DEBUG is specified with "python manage.py runserver -d", this is
    not needed.

--- config.py
+++ config.py
@@ -11,6 +11,3 @@ SECRET_KEY = '73Sjkadas%hASDe45@1238Due12jdodjfFDweq82348*(#89asdasdas'
 basedir = path.abspath(path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + path.join(basedir, 'swagmail.db')
 SQLALCHEMY_MIGRATE_REPO = path.join(basedir, 'db_repository')
-
-""" Dev """
-DEBUG = True
