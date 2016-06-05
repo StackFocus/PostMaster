8bd183ee65b9e2bea29df9f2a36a200bad67ea40
PrahlM93 <matt_prahl@live.com>
2016-06-05 12:40:21 -0400

    Remove unused functions

--- postmaster/utils.py
+++ postmaster/utils.py
@@ -15,57 +15,6 @@ from postmaster import db, models, bcrypt
 from postmaster.errors import ValidationError
 
 
-def row2dict(row):
-    """ Return a dictionary from a sqlalchemy row We use this so
-        we don't have to deal with sa_instance_state stuff
-    """
-    d = {}
-    for column in row.__table__.columns:
-        d[column.name] = str(getattr(row, column.name))
-    return d
-
-
-def getAllFromTable(table):
-    """ Returns the entire table that is specified
-    """
-    # Dynamically get the model to query based on the table variable
-    if getattr(models, table):
-        table = (getattr(models, table)).query.all()
-        listItemsDict = [row2dict(row) for row in table]
-        return listItemsDict
-
-    return None
-
-
-def getDomain(name):
-    """ Returns a domain from VirtualDomains
-    """
-    queriedDomain = models.VirtualDomains.query.filter_by(name=name).first()
-    if queriedDomain:
-        return row2dict(queriedDomain)
-    return None
-
-
-def getUser(email):
-    """ Returns a user from VirtualUsers
-    """
-    queriedUser = models.VirtualUsers.query.filter_by(email=email).first()
-
-    if queriedUser:
-        return row2dict(queriedUser)
-    return None
-
-
-def getAlias(source):
-    """ Returns alias from VirtualAliases
-    """
-    queriedAlias = models.VirtualAliases.query.filter_by(source=source).first()
-
-    if queriedAlias:
-        return row2dict(queriedAlias)
-    return None
-
-
 def maildb_auditing_enabled():
     """ Returns a bool based on if mail db auditing is enabled
     """
--- tests/utils/test_utils_functions.py
+++ tests/utils/test_utils_functions.py
@@ -8,20 +8,6 @@ from postmaster.apiv1.utils import *
 
 class TestUtilsFunctions:
 
-    def test_getDomain(self):
-        result = getDomain('postmaster.com')
-        assert (result['name'] == 'postmaster.com') and ('id' in result)
-
-    def test_getUser(self):
-        result = getUser('email@postmaster.com')
-        assert (result['email'] == 'email@postmaster.com') and (
-            'id' in result) and ('password' in result)
-
-    def test_getAlias(self):
-        result = getAlias('aliasemail@postmaster.com')
-        assert (result['source'] == 'aliasemail@postmaster.com') and (
-            result['destination'] == 'email@postmaster.com')
-
     def test_maildb_auditing_enabled(self):
         result = maildb_auditing_enabled()
         assert isinstance(result, bool)
