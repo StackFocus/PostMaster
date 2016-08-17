f28e9efdc2e9d54577acc6eae2962654dd7d068f
PrahlM93 <matt_prahl@live.com>
2016-08-16 22:02:18 -0400

    Added the ability to specify a SECRET_KEY when running Docker, and added Docker documentation

--- config.default.py
+++ config.default.py
@@ -12,7 +12,7 @@ class BaseConfiguration(object):
     # We introduce very little risk by disabling this.
     WTF_CSRF_ENABLED = False
     # Make this random (used to generate session keys)
-    SECRET_KEY = 'e9987dce48df3ce98542529fd074d9e9f9cd40e66fc6c4c2'
+    SECRET_KEY = '123456789abcdef123456789'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = 'mysql://root:vagrant@localhost:3306/servermail'
     basedir = path.abspath(path.dirname(__file__))
