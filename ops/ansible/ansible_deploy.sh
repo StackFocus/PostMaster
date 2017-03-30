3a1d6e6efb30b8b3f14e4d79d2f25aa78bf049ea
mprahl <matt_prahl@live.com>
2017-03-30 18:35:52 -0400

    Move 'ansible-galaxy install' command to TravisCI's 'before_deploy' section

--- ops/ansible/ansible_deploy.sh
+++ ops/ansible/ansible_deploy.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-rm -rf /opt/postmaster/git/ops/ansible/roles
-mkdir /opt/postmaster/git/ops/ansible/roles
-pip install --upgrade "ansible>=2.0.0.1" setuptools
-# TODO: Should this run by TravisCI instead and included in the .deb package?
-ansible-galaxy install -r /opt/postmaster/git/ops/ansible/requirements.yml -p /opt/postmaster/git/ops/ansible/roles/
 ansible-playbook -i "localhost," -e postmaster_deb_package=True -c local /opt/postmaster/git/ops/ansible/deploy.yml
